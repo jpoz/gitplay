ba0688f8ac0562c91643bad027b1c800c9650c5b
JPoz <jpoz@jpoz.net>
2010-07-01 19:30:06 -0700

    adding more stuff

--- main.rb
+++ main.rb
@@ -4,3 +4,9 @@ require 'sinatra'
 get '/' do
   'hello'
 end
+
+set :sass, {:style => :compact } # default Sass style is :nested
+get '/stylesheet.css' do
+  header 'Content-Type' => 'text/css; charset=utf-8'
+  sass :stylesheet
+end
